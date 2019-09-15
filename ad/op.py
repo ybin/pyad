550317f0dece462812b2a69373925a3eaab5ebbb
ybin <ybin.sun@163.com>
2019-09-15 20:53:28 +0800

    nn

--- ad/op.py
+++ ad/op.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Op:
     def __init__(self, name):
         self.name = name
@@ -35,3 +38,31 @@ class DivOp(Op):
             inputs[0].grad += 1 / inputs[1].value
         if inputs[1].require_grad:
             inputs[1].grad += -1 * inputs[0].value
+
+
+class LinearOp(Op):
+    """
+    Y = X * W + B
+    """
+
+    def backward(self, grad, inputs):
+        x = inputs[0]
+        W = inputs[1]
+        b = inputs[2]
+        if x.require_grad:
+            x.grad += np.dot(grad, W.value)
+        if W.require_grad:
+            W.grad = 1
+        if b.require_grad:
+            b.grad += grad * np.ones_like(grad)
+        pass
+
+
+class ConvOp(Op):
+    def backward(self, grad, inputs):
+        pass
+
+
+class AvgPoolingOp(Op):
+    def backward(self, grad, inputs):
+        pass
